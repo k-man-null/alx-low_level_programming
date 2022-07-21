e01e1643257131a34965899c8ba88313bdf9d080
k-man-null <williamoriginal2019@gmail.com>
2022-07-21 09:49:13 -0700

    Completed 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints out every possible combination of single digit numbers
+ * Return: zero for success and other for error
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+
+	{
+		putchar(i);
+		if (i == '9')
+		{
+			break;
+		}
+		else
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	return (0);
+}
