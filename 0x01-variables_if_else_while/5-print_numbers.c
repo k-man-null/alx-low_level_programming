5714e5ef06a76ec9d2f48e98f4ca0a9551e8b521
k-man-null <williamoriginal2019@gmail.com>
2022-07-21 06:55:57 -0700

    Completed 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints single dig numbers starting for 0 to 9 
+ * number stored in the variable i
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+	
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return(0);
+}
