77935958d7615b5b05129763d3749bd794bc673d
k-man-null <williamoriginal2019@gmail.com>
2022-07-21 07:26:14 -0700

    Completed 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints single dig numbers starting for 0 to 9 
+ * main - prints single dig numbers starting for 0 to 9
  * number stored in the variable i
  * Return: Always 0 (Success)
  */
@@ -9,13 +9,13 @@
 int main(void)
 {
 	int i;
+
 	i = 0;
-	
 	while (i <= 9)
 	{
 		printf("%d", i);
 		i++;
 	}
 	printf("\n");
-	return(0);
+	return (0);
 }
