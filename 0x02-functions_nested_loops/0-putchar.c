e282369e85456a3a699a8f6a4e75c7a7476cf423
k-man-null <williamoriginal2019@gmail.com>
2022-07-23 11:53:35 -0700

    first try

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main-program entry point
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	char letters[9] = "_putchar\n";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(letters[i]);
+	}
+	return (0);
+}
