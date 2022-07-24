8eeeced4341d997859bc2ea87ba3fbaed475f0e1
k-man-null <williamoriginal2019@gmail.com>
2022-07-23 17:44:07 -0700

    Completed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,12 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char c;
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar(print_alphabet());
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
 	}
 	_putchar('\n');
 }
