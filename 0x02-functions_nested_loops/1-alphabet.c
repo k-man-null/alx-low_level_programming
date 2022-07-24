c84ef7b41410893e7eddadc14d55e88c7d4b3dce
k-man-null <williamoriginal2019@gmail.com>
2022-07-23 17:16:14 -0700

    Completed 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
- * Return: Always 0.
+ * Return: void.
  */
 
-void print_alphabet(void);
-
-int  main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
-{
-	for (int i = 'a'; i <= 'z'; i++)
+{	
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
